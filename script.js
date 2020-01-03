c98c56fa7ef0297d06c2e70052193574861fe001
Dave Edwards <dave@dwedigital.com>
2020-01-03 13:36:35 +0000

    added randomizer & button

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ function setGradient(){
     currentColor.textContent = body.style.background
 }
 
+
+
+function randomize(){
+    color1.value = '#'+Math.floor(Math.random()*16777215).toString(16);
+    color2.value = '#'+Math.floor(Math.random()*16777215).toString(16);
+    setGradient();
+}
+
 color1.addEventListener('input', setGradient)
 
 color2.addEventListener('input', setGradient)
 
 document.addEventListener('DOMContentLoaded',setGradient)
+document.querySelector('button').addEventListener('click', randomize)
